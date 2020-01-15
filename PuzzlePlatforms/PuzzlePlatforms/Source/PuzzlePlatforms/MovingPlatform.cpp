58ee6d0e722c708fc632be8404d0fee3b36d820c
Andrew Rimpici <AndrewR608@gmail.com>
2020-01-15 15:19:57 -0500

    Server-Client moving platform in one direction

---
+++ PuzzlePlatforms/PuzzlePlatforms/Source/PuzzlePlatforms/MovingPlatform.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MovingPlatform.h"
+
+AMovingPlatform::AMovingPlatform()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	SetMobility(EComponentMobility::Movable);
+}
+
+void AMovingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//Only valid for servers to replicate objects.
+	if (HasAuthority())
+	{
+		//Allows for this object to be replicated by the server.
+		SetReplicates(true);
+
+		//Replicates the movement of this object since it doesn't by default.
+		SetReplicateMovement(true);
+	}
+}
+
+void AMovingPlatform::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	if (HasAuthority())
+	{
+		FVector Location = GetActorLocation();
+		FVector GlobalTargetLocation = GetTransform().TransformPosition(TargetLocation);
+		FVector Direction = (GlobalTargetLocation - Location).GetSafeNormal();
+		Location += Direction * Speed * DeltaSeconds;
+		SetActorLocation(Location);
+	}
+}
